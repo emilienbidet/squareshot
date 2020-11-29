f9705f04624ebd29141307c5310e01d9254353f5
emilienbidet <emilienbdt@gmail.com>
2020-11-30 00:15:03 +0100

    add collisions detection

--- src/game_state.py
+++ src/game_state.py
@@ -5,18 +5,43 @@ from src.wall import *
 class GameState:
 
     def __init__(self):
-        self.player = Player(0)
-        self.walls = [Wall(0,3), Wall(1,3), Wall(2,3), Wall(2,4), Wall(3,1), Wall(4,1), Wall(4,2),Wall(4,3)]
+        self.player_1 = Player(0, self)
+        self.player_2 = Player(1, self)
+        self.walls = pygame.sprite.Group()
 
+        for y in [0,11]:
+            for x in range(12):
+                self.walls.add(Wall(y,x))
 
-    def update(self):
-        self.player.update()
+    def update(self, player_1_state, player_2_state):
+        self.player_1.update(player_1_state)
+        self.player_2.update(player_2_state)
+        GameState.remove_bullets_collided(self.player_1.bullets, self.player_2.bullets, self.walls)
+        GameState.remove_bullets_on_player(self.player_1, self.player_2)
 
     def draw(self, window):
         window.fill(GameConfig.COLOR_BLUE)
-        self.player.draw(window)
-        self.draw_walls(window)
+        self.player_1.draw(window)
+        self.player_2.draw(window)
+        self.player_1.bullets.draw(window)
+        self.player_2.bullets.draw(window)
+        GameState.draw_all(window, self.walls)
 
-    def draw_walls(self,window):
-        for wall in self.walls:
-            wall.draw(window)
+    def draw_all(window, group):
+        for element in group:
+            element.draw(window)
+
+    def check_collisions(self, sprite, group):
+        return pygame.sprite.spritecollideany(sprite, group)
+
+    def remove_bullets_on_player(player_1, player_2):
+        pygame.sprite.spritecollide(player_1, player_2.bullets, True)
+        pygame.sprite.spritecollide(player_2, player_1.bullets, True)
+
+    def remove_bullets_collided(player_1_bullets, player_2_bullets, walls):
+        """
+            Remove Bullets when they are collide or when they are in a wall
+        """
+        pygame.sprite.groupcollide(player_1_bullets, player_2_bullets, True, True)
+        pygame.sprite.groupcollide(player_1_bullets, walls, True, False)
+        pygame.sprite.groupcollide(player_2_bullets, walls, True, False)
