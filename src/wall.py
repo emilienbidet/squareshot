4af55299acdcccc2ee88beb7c2deac0bfb5679dd
emilienbidet <emilienbdt@gmail.com>
2020-11-30 00:08:40 +0100

    move configs into he config file

--- src/wall.py
+++ src/wall.py
@@ -1,17 +1,12 @@
 import pygame
+from src.game_config import *
 
 class Wall(pygame.sprite.Sprite):
 
-    WIDTH = 50
-    HEIGHT = 50
-
-    IMAGE = pygame.image.load("media/wall.png")
-
     def __init__(self, x, y):
-
-        pygame.sprite.Sprite.__init__(self)
-        self.image = Wall.IMAGE
-        self.rect = pygame.Rect(50*x, 50*y, Wall.WIDTH, Wall.HEIGHT)
+        super().__init__()
+        self.image = pygame.image.load(GameConfig.WALL_IMAGE[0])
+        self.rect = pygame.Rect(GameConfig.WALL_WIDTH*x, GameConfig.WALL_HEIGHT*y, GameConfig.WALL_WIDTH, GameConfig.WALL_HEIGHT)
 
     def draw(self,window):
         window.blit(self.image ,self.rect.topleft)
