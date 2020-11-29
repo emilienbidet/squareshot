7f91fa18c9e37ead1384a024e4e602c3768503a1
emilienbidet <emilienbdt@gmail.com>
2020-11-30 00:16:02 +0100

    regroup all configs into the class

--- src/game_config.py
+++ src/game_config.py
@@ -4,6 +4,26 @@ class GameConfig:
     WINDOW_WIDTH = 600
 
     COLOR_BLUE = (161, 237, 242)
+    BAR_COLOR = (110,210,46)
+    BAR_HEIGHT = 5
 
-    IMAGE_FOR_PLAYERS = ["media/player.png", "media/ia.png"]
-    IMAGE_FOR_BULLETS = ["media/bullet-player.png", "media/bullet-ia.png"]
+    PLAYER_IMAGE = ["media/player.png", "media/ia.png"]
+    BULLET_IMAGE = ["media/bullet-player.png", "media/bullet-ia.png"]
+    WALL_IMAGE = ["media/wall.png"]
+
+    PLAYER_HEIGHT = 40
+    PLAYER_WIDTH = PLAYER_HEIGHT
+
+    PLAYER_STATE_MOVES = {"UP": -1,
+                          "DOWN": 1,
+                          "RIGHT": 1,
+                          "LEFT": -1}
+
+    WALL_HEIGHT = 50
+    WALL_WIDTH = WALL_HEIGHT
+
+    BULLET_HEIGHT = 20
+    BULLET_WIDTH = BULLET_HEIGHT
+
+    PLAYER_SPEED = 4
+    BULLET_SPEED = 6
