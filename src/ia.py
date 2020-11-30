a12011d90f8b3471d8fb7ae45fb49fed805cdc92
emilienbidet <emilienbdt@gmail.com>
2020-11-30 23:43:36 +0100

    add IA movement

--- src/ia.py
+++ src/ia.py
@@ -1,4 +1,7 @@
 from src.player_state import *
+import random
+import pygame
+from src.game_config import *
 
 class IA(object):
     """docstring for IA."""
@@ -8,12 +11,32 @@ class IA(object):
     def __init__(self, game_state):
         self.state = IA.STATES[0]
         self.game_state = game_state
+        self.last = pygame.time.get_ticks()
+        self.last_state = PlayerState()
 
     def get_next_ia_state(self):
-        next_ia_state = PlayerState()
-        next_ia_state.shot = True
-        next_ia_state.mouse_pos = self.shot_to_player()
-        return next_ia_state
+        now = pygame.time.get_ticks()
+        if now - self.last >= 200:
+            self.last = now
+            next_ia_state = PlayerState()
+            next_ia_state.shot = True
+            next_ia_state.mouse_pos = self.shot_to_player()
+
+            if bool(random.getrandbits(1)):
+                next_ia_state.run = True
+
+            if bool(random.getrandbits(1)):
+                next_ia_state.up = True
+            else:
+                next_ia_state.down = True
+            if bool(random.choice([0,0,0,0,0,1])):
+                next_ia_state.right = True
+            else:
+                next_ia_state.left = True
+            self.last_state = next_ia_state
+            return next_ia_state
+        else:
+            return self.last_state
 
     def shot_to_player(self):
         return self.game_state.player_1.rect.center
