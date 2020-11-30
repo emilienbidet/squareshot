35882110bc358e76e3b117008a2d767511ad3952
emilienbidet <emilienbdt@gmail.com>
2020-11-30 23:44:07 +0100

    add comments

--- src/bullet.py
+++ src/bullet.py
@@ -5,6 +5,9 @@ import math
 class Bullet(pygame.sprite.Sprite):
 
     def __init__(self, player, player_type, mouse_pos):
+        """
+            Method to create a new Bullet
+        """
         super().__init__()
         self.player = player
         # Bullet's hitbox
@@ -24,13 +27,22 @@ class Bullet(pygame.sprite.Sprite):
         window.blit(self.image ,self.rect.topleft)
 
     def get_angle(self, mouse_pos):
+        """
+            Method to get the angle in radian
+        """
         mouse_x, mouse_y = mouse_pos
         player_x, player_y = self.player.rect.x + GameConfig.PLAYER_WIDTH/4, self.player.rect.y + GameConfig.PLAYER_HEIGHT/4
         return math.atan2(mouse_y-player_y, mouse_x-player_x)
 
     def update(self):
+        """
+            Method to update the bullet
+        """
         self.move()
 
     def move(self):
+        """
+            Method to move the bullet according to its angle
+        """
         self.rect.x += int(math.cos(self.angle)*GameConfig.BULLET_SPEED)
         self.rect.y += int(math.sin(self.angle)*GameConfig.BULLET_SPEED)
